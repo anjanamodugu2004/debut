374a430940e562987edbd53d816c39a8506959e2
Anjana Modugu <anjanamodugu@gmail.com>
2025-01-02 19:48:02 +0530

    door handle partial fixed

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -1,72 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace SojaExiles
-
 {
-	public class opencloseDoor : MonoBehaviour
-	{
+    public class opencloseDoor : MonoBehaviour
+    {
+        public Animator openandclose;
+        public bool open;
+        public Transform Player;
+        public XRController leftController;
+        public XRController rightController;
 
-		public Animator openandclose;
-		public bool open;
-		public Transform Player;
+        private InputAction interactAction;
 
-		void Start()
-		{
-			open = false;
-		}
+        void Start()
+        {
+            open = false;
 
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
+            // Set up the input action for interacting (assuming "Activate" is mapped to the correct button in the input system)
+            interactAction = new InputAction(type: InputActionType.Button, binding: "<XRController>{RightHand}/trigger");
+            interactAction.Enable();
+        }
 
-						}
+        void Update()
+        {
+            if (Player)
+            {
+                float dist = Vector3.Distance(Player.position, transform.position);
+                if (dist < 15)
+                {
+                    if (IsInteractionButtonPressed())
+                    {
+                        if (open == false)
+                        {
+                            StartCoroutine(opening());
+                        }
+                        else
+                        {
+                            StartCoroutine(closing());
+                        }
+                    }
+                }
+            }
+        }
 
-					}
-				}
+        private bool IsInteractionButtonPressed()
+        {
+            // Check if the interaction button is pressed
+            return interactAction.WasPressedThisFrame();
+        }
 
-			}
+        IEnumerator opening()
+        {
+            print("you are opening the door");
+            openandclose.Play("Opening");
+            open = true;
+            yield return new WaitForSeconds(.5f);
+        }
 
-		}
-
-		IEnumerator opening()
-		{
-			print("you are opening the door");
-			openandclose.Play("Opening");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			openandclose.Play("Closing");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
-	}
+        IEnumerator closing()
+        {
+            print("you are closing the door");
+            openandclose.Play("Closing");
+            open = false;
+            yield return new WaitForSeconds(.5f);
+        }
+    }
 }
